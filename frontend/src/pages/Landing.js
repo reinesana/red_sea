1263e7031bb18ce0df1fc5412bb67122c1c68b3c
ksskyy <149976697+ksskyy@users.noreply.github.com>
2024-03-10 06:44:29 -0700

    update landing page style

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -4,9 +4,16 @@ import { Link } from "react-router-dom";
 function Landing() {
   return (
     <div className="landing">
-      <p>welcome to Red Sea</p>
-      <h3>A safe passage in the endless war</h3>
       <Earth />
+      <div className="welcome">
+        <p>welcome to Red Sea</p>
+        <h3>A safe passage in the endless war</h3>
+      </div>
+      <input
+        type="text"
+        placeholder="Enter your location"
+        className="landing-input"
+      />
       <Link to="/map">
         <button className="landing-btn">Start</button>
       </Link>
