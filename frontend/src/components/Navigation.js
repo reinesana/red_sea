2f4b5ab42487e52e09985e53f3c982e1c1affae7
Emily He <emmyemmyhe@gmail.com>
2024-03-09 18:31:12 -0800

    made variables partial file

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -5,7 +5,6 @@ function Navigation() {
     <div className="navigation">
       <nav className="thumb-nav">
         <ul className="nav-menu">
-          <li>Home</li>
           <li>Map</li>
           <li>Help</li>
           <li>Women's<br></br> Safety</li>
