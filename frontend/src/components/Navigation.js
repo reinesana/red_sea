c97cdfde5298809276d25b1fd96a39909f8816a3
Emily He <emmyemmyhe@gmail.com>
2024-03-09 20:19:50 -0800

    navigation 90% done

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,15 +1,28 @@
 import { NavLink } from "react-router-dom";
-import "../styles/components/_navigation.scss";
-
+import { IoIosHelpCircleOutline } from "react-icons/io";
+import { FaMapPin } from "react-icons/fa";
+import { MdOutlineHealthAndSafety } from "react-icons/md";
 
 function Navigation() {
   return (
     <div className="navigation">
       <nav className="thumb-nav">
         <ul className="nav-menu">
-          <li><NavLink to="/">Map</NavLink></li>
-          <li><NavLink to="/help">Help</NavLink></li>
-          <li><NavLink to="/safety-for-women">Women's<br></br> Safety</NavLink></li>
+          <li className="map-li">
+            <FaMapPin className="map-icon" />
+            <br></br>
+            <NavLink to="/">Map</NavLink>
+          </li>
+          <li className="help-li">
+            <IoIosHelpCircleOutline className="help-icon" />
+            <br></br>
+            <NavLink to="/help">Help</NavLink>
+          </li>
+          <li>
+            <MdOutlineHealthAndSafety className="safety-icon"/>
+            <br></br>
+            <NavLink to="/safety-for-women">Women's Safety</NavLink>
+          </li>
         </ul>
       </nav>
     </div>
