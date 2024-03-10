3ab7beb9c150994835a571bef6e63eef66188d68
ksskyy <149976697+ksskyy@users.noreply.github.com>
2024-03-09 17:16:43 -0800

    add header

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header>
+      <h1>My App</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Map</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
