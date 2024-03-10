de035cd900d9bd477b923df8fe985ab6f13fd85c
ksskyy <149976697+ksskyy@users.noreply.github.com>
2024-03-09 23:21:16 -0800

    add btn on landing page

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,10 +1,14 @@
 import Earth from "../components/Earth";
+import { Link } from "react-router-dom";
 
 function Landing() {
   return (
     <div>
       <h1>Welcome to the Landing Page</h1>
       <Earth />
+      <Link to="/map">
+        <button>Start</button>
+      </Link>
     </div>
   );
 }
