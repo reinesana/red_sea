2dfc4525e0a80101a269c2972c91581308e113d7
ksskyy <149976697+ksskyy@users.noreply.github.com>
2024-03-09 21:56:11 -0800

    Landing page, 3d earth

---
+++ frontend/src/pages/Landing.js
@@ -0,0 +1,11 @@
+import Earth from "../components/Earth";
+
+function Landing() {
+  return (
+    <div>
+      <h1>Welcome to the Landing Page</h1>
+      <Earth />
+    </div>
+  );
+}
+export default Landing;
--- frontend/src/router/AppRouter.js
+++ frontend/src/router/AppRouter.js
@@ -3,21 +3,22 @@ import Header from "../components/Navigation";
 import Help from "../pages/Help";
 import Map from "../pages/Map";
 import WomenSafety from "../pages/WomenSafety";
+import Landing from "../pages/Landing";
 
 function AppRouter() {
-
-    return (
-        <BrowserRouter basename="/">
-                <Header />
-                <div className="main-wrapper">
-                    <Routes>
-                        <Route path="/" element={<Map/>} />
-                        <Route path="/help" element={<Help />} />
-                        <Route path="/safety-for-women" element={<WomenSafety />} />
-                    </Routes>
-                </div>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter basename="/">
+      <Header />
+      <div className="main-wrapper">
+        <Routes>
+          <Route path="/" element={<Map />} />
+          <Route path="/help" element={<Help />} />
+          <Route path="/safety-for-women" element={<WomenSafety />} />
+          <Route path="/landing" element={<Landing />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default AppRouter;
