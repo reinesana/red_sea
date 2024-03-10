b8b6e4dd91edd470751c65d4694e942a338f5cae
Emily He <emmyemmyhe@gmail.com>
2024-03-10 04:53:47 -0700

    style updates

--- frontend/src/pages/Help.js
+++ frontend/src/pages/Help.js
@@ -1,79 +1,130 @@
-function Help (){
+function Help() {
+  return (
+    <div className="help">
+      <h1>Help Centers</h1>
+      <div>
+        <h3>
+          United Nations Relief and Works Agency for Palestine Refugees in the
+          Near East (UNRWA)
+        </h3>
+        <p>
+          Website: <a href="https://www.unrwa.org">https://www.unrwa.org</a>
+        </p>
+        <p>Phone: +970 5 979 20223</p>
+      </div>
 
-    return(
-        <div className="help">
-            <h1>Help Centers</h1>
-            <div>
-                <h3>United Nations Relief and Works Agency for Palestine Refugees in the Near East (UNRWA)</h3>
-                <p>Website: <a href="https://www.unrwa.org">https://www.unrwa.org</a></p>
-                <p>Phone: +970 5 979 20223</p>
-            </div>
+      <div>
+        <h3>Palestine Red Crescent Society</h3>
+        <p>
+          Website:{" "}
+          <a href="http://www.palestinercs.org/">
+            http://www.palestinercs.org/
+          </a>
+        </p>
+        <p>Email: info@palestinercs.org</p>
+        <p>Phone: +970 8 282 0445</p>
+      </div>
 
-            <div>
-                <h3>Palestine Red Crescent Society</h3>
-                <p>Website: <a href="http://www.palestinercs.org/">http://www.palestinercs.org/</a></p>
-                <p>Email: info@palestinercs.org</p>
-                <p>Phone: +970 8 282 0445</p>
-            </div>
+      <div>
+        <h3>UNICEF (United Nations Children's Fund)</h3>
+        <p>
+          Website: <a href="https://www.unicef.org/">https://www.unicef.org/</a>
+        </p>
+        <p>Email: gaza@unicef.org</p>
+        <p>Phone: +970 8 288 1800</p>
+      </div>
 
-            <div>
-                <h3>UNICEF (United Nations Children's Fund)</h3>
-                <p>Website: <a href="https://www.unicef.org/">https://www.unicef.org/</a></p>
-                <p>Email: gaza@unicef.org</p>
-                <p>Phone: +970 8 288 1800</p>
-            </div>
+      <div>
+        <h3>International Committee of the Red Cross (ICRC)</h3>
+        <p>
+          Website: <a href="https://www.icrc.org/">https://www.icrc.org/</a>
+        </p>
+        <p>Email: gaza@icrc.org</p>
+        <p>Phone: +972 8 282 3056</p>
+      </div>
 
-            <div>
-                <h3>International Committee of the Red Cross (ICRC)</h3>
-                <p>Website: <a href="https://www.icrc.org/">https://www.icrc.org/</a></p>
-                <p>Email: gaza@icrc.org</p>
-                <p>Phone: +972 8 282 3056</p>
-            </div>
+      <div>
+        <h3>Oxfam</h3>
+        <p>
+          Website: <a href="https://www.oxfam.org/">https://www.oxfam.org/</a>
+        </p>
+        <p>Email: info@oxfam.org</p>
+        <p>Phone: +44 1865 473727</p>
+      </div>
 
-            <div>
-                <h3>Oxfam</h3>
-                <p>Website: <a href="https://www.oxfam.org/">https://www.oxfam.org/</a></p>
-                <p>Email: info@oxfam.org</p>
-                <p>Phone: +44 1865 473727</p>
-            </div>
+      <div>
+        <h3>Doctors Without Borders (Médecins Sans Frontières)</h3>
+        <p>
+          Website:<a href="https://www.msf.org/">https://www.msf.org/</a>
+        </p>
+        <p>
+          Contact Form:{" "}
+          <a href="https://www.msf.org/contact-us">
+            https://www.msf.org/contact-us
+          </a>
+        </p>
+        <p>Phone: +962 6 462 4775</p>
+      </div>
 
-            <div>
-                <h3>Doctors Without Borders (Médecins Sans Frontières)</h3>
-                <p>Website:<a href="https://www.msf.org/">https://www.msf.org/</a></p>
-                <p>Contact Form: <a href="https://www.msf.org/contact-us">https://www.msf.org/contact-us</a></p>
-                <p>Phone: +962 6 462 4775</p>
-            </div>
+      <div>
+        <h3>Save the Children</h3>
+        <p>
+          Website:{" "}
+          <a href="https://www.savethechildren.net/">
+            https://www.savethechildren.net/
+          </a>
+        </p>
+        <p>Email: info@savethechildren.org</p>
+        <p>Phone: +962 6 593 2590</p>
+      </div>
 
-            <div>
-                <h3>Save the Children</h3>
-                <p>Website: <a href="https://www.savethechildren.net/">https://www.savethechildren.net/</a></p>
-                <p>Email: info@savethechildren.org</p>
-                <p>Phone: +962 6 593 2590</p>
-            </div>
+      <div>
+        <h3>Action Against Hunger (Action Contre la Faim)</h3>
+        <p>
+          Website:{" "}
+          <a href="https://www.actionagainsthunger.org/">
+            https://www.actionagainsthunger.org/
+          </a>
+        </p>
+        <p>
+          Contact Form:{" "}
+          <a href="https://www.actionagainsthunger.org/contact-us">
+            https://www.actionagainsthunger.org/contact-us
+          </a>
+        </p>
+        <p>Phone: +33 1 43 35 88 88</p>
+      </div>
 
-            <div>
-                <h3>Action Against Hunger (Action Contre la Faim)</h3>
-                <p>Website: <a href="https://www.actionagainsthunger.org/">https://www.actionagainsthunger.org/</a></p>
-                <p>Contact Form: <a href="https://www.actionagainsthunger.org/contact-us">https://www.actionagainsthunger.org/contact-us</a></p>
-                <p>Phone: +33 1 43 35 88 88</p>
-            </div>
+      <div>
+        <h3>Islamic Relief</h3>
+        <p>
+          Website:{" "}
+          <a href="https://www.islamic-relief.org/">
+            https://www.islamic-relief.org/
+          </a>
+        </p>
+        <p>Email: info@islamic-relief.org</p>
+        <p>Phone: +44 121 633 6275</p>
+      </div>
 
-            <div>
-                <h3>Islamic Relief</h3>
-                <p>Website: <a href="https://www.islamic-relief.org/">https://www.islamic-relief.org/</a></p>
-                <p>Email: info@islamic-relief.org</p>
-                <p>Phone: +44 121 633 6275</p>
-            </div>
-
-            <div>
-                <h3>International Medical Corps</h3>
-                <p>Website: <a href="https://internationalmedicalcorps.org/">https://internationalmedicalcorps.org/</a></p>
-                <p>Contact Form: <a href="https://internationalmedicalcorps.org/contact/">https://internationalmedicalcorps.org/contact/</a></p>
-                <p>Phone: +1 424 252 6000</p>
-            </div>
-            
-        </div>
-    )
+      <div>
+        <h3>International Medical Corps</h3>
+        <p>
+          Website:{" "}
+          <a href="https://internationalmedicalcorps.org/">
+            https://internationalmedicalcorps.org/
+          </a>
+        </p>
+        <p>
+          Contact Form:{" "}
+          <a href="https://internationalmedicalcorps.org/contact/">
+            https://internationalmedicalcorps.org/contact/
+          </a>
+        </p>
+        <p>Phone: +1 424 252 6000</p>
+      </div>
+    </div>
+  );
 }
 
 export default Help;
