d47a15efc292eca257175deec6b9775d008e2adf
Emily He <emmyemmyhe@gmail.com>
2024-03-09 21:36:30 -0800

    thumb navigation route and content update

--- frontend/src/router/AppRouter.js
+++ frontend/src/router/AppRouter.js
@@ -1,23 +1,22 @@
 import { BrowserRouter, Route, Routes, GlobalProvider } from "react-router-dom";
-import Header from "../components/Navigation";
+import Navigation from "../components/Navigation";
 import Help from "../pages/Help";
 import Map from "../pages/Map";
 import WomenSafety from "../pages/WomenSafety";
 
 function AppRouter() {
-
-    return (
-        <BrowserRouter basename="/">
-                <Header />
-                <div className="main-wrapper">
-                    <Routes>
-                        <Route path="/" element={<Map/>} />
-                        <Route path="/help" element={<Help />} />
-                        <Route path="/safety-for-women" element={<WomenSafety />} />
-                    </Routes>
-                </div>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter className="body-wrapper" basename="/">
+      <Navigation className="main-navigation" />
+      <div className="main-wrapper">
+        <Routes>
+          <Route path="/" element={<Map />} />
+          <Route path="/help-centers" element={<Help />} />
+          <Route path="/safety-for-women" element={<WomenSafety />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default AppRouter;
