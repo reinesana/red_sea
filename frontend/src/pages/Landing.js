c0d30b7434dda6c95d3653430bdcbcde94348dde
ksskyy <149976697+ksskyy@users.noreply.github.com>
2024-03-10 01:52:19 -0800

    update landing

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 function Landing() {
   return (
     <div className="landing">
-      <h1>Welcome to the Landing Page</h1>
+      <p>welcome to Red Sea</p>
+      <h3>A safe passage in the endless war</h3>
       <Earth />
       <Link to="/map">
         <button className="landing-btn">Start</button>
