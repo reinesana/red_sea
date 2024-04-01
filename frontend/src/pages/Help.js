0da89b5912319c645d842ccad7c7f2b1c824d621
reinesana <153279125+reinesana@users.noreply.github.com>
2024-04-01 00:48:19 -0700

    updating help center section

--- frontend/src/pages/Help.js
+++ frontend/src/pages/Help.js
@@ -8,42 +8,48 @@ const helpCenters = [
     description: "The United Nations Relief and Works Agency for Palestine Refugees in the Near East is a UN agency that supports the relief and human development of Palestinian refugees.",
     website: "https://www.unrwa.org",
     phone: "+970 5 979 20223",
-    categories: ["Humanitarian"]
+    categories: ["Humanitarian"],
+    imageUrl: "background1.png"
   },
   {
     name: "Palestine Red Crescent Society",
     description: "The Palestine Red Crescent Society is the humanitarian organization that is the International Red Cross and Red Crescent Movement in the State of Palestine, which includes the West Bank, including East Jerusalem, and the Gaza Strip.",
     website: "http://www.palestinercs.org",
     phone: "+970 8 282 0445",
-    categories: ["Humanitarian", "Medical"]
+    categories: ["Humanitarian", "Medical"],
+    imageUrl: "background2.png"
   },
   {
     name: "UNICEF (United Nations Children's Fund)",
     description: "UNICEF, originally called the United Nations International Children's Emergency Fund in full, now officially United Nations Children's Fund, is an agency of the United Nations responsible for providing humanitarian and developmental aid to children worldwide.",
     website: "https://www.unicef.org",
     phone: "+970 8 288 1800",
-    categories: ["Humanitarian"]
+    categories: ["Humanitarian"],
+    imageUrl: "https://example.com/unrwa-image.jpg"
   },
   {
     name: "International Committee of the Red Cross (ICRC)",
     description: "Present in Israel and the occupied territories since 1967 and work with the Palestine Red Crescent Society and Magen David Adom in Israel. The ICRC has offices in Tel Aviv, the West Bank and Gaza.",
     website: "https://www.icrc.org",
     phone: "+972 8 282 3056",
-    categories: ["Humanitarian", "Medical"]
+    categories: ["Humanitarian", "Medical"],
+    imageUrl: "https://example.com/unrwa-image.jpg"
   },
   {
     name: "Oxfam",
     description: "Oxfam is a British-founded confederation of 21 independent non-governmental organizations NGOs, focusing on the alleviation of global poverty, founded in 1995 and led by Oxfam International.",
     website: "https://www.oxfam.org",
     phone: "+44 1865 473727",
-    categories: ["Poverty"]
+    categories: ["Poverty"],
+    imageUrl: "https://example.com/unrwa-image.jpg"
   },
   {
     name: "Doctors Without Borders",
     description: "Médecins Sans Frontières, also known as Doctors Without Borders, is a charity that provides humanitarian medical care.",
     website: "https://www.msf.org",
     phone: "+962 6 462 4775",
-    categories: ["Medical"]
+    categories: ["Medical"],
+    imageUrl: "https://example.com/unrwa-image.jpg"
   }
 ];
 
@@ -85,7 +91,11 @@ function Help() {
         </div>
         <div className="help-centers-container">
           {filterHelpCenters.map((center, index) => (
-            <div key={index} className="help-center">
+            <div 
+            key={index} 
+            className="help-center" 
+            style={{ backgroundImage: `url(${center.imageUrl})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+          >
               <p><FaExternalLinkAlt /> <a href={center.website}>Website</a></p>
               <p>{center.phone ? <><FaPhoneAlt /> {center.phone}</> : <><LuContact2 /> <a href={center.website}>Contact</a></>}</p>
               <h3>{center.name}</h3>
